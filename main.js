d3538d586a803528057e4c741f0d72fa8817df44
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-10-28 16:03:58 -0400

    getting shape to hide with js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var shape = document.getElementById('shape');
+
 console.log ("test");
 function shapeDisplay () {
+  var shape = document.getElementById('shape');
+  console.log ("function ran");
   if (shape.style.display === "none") {
     shape.style.display = "block";
   } else {
