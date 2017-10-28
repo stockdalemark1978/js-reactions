387a8577e17f1fb374876deeacf74c7b4806f1f5
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-10-28 16:14:07 -0400

    added time measurement

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ function shapeDisplay () {
   var shape = document.getElementById('shape');
 
   var endTime = new Date().getTime();
-  
+
   if (shape.style.display === "none") {
     shape.style.display = "block";
   } else {
     shape.style.display = "none";
   }
+
+  var timeTaken = endTime - startTime;
+  console.log (timeTaken);
 }
