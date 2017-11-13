43e9143330859e7ba7e494f58e9466850617c825
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-11-12 19:20:04 -0500

    trying to get block to reappear

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 var startTime = new Date().getTime();
+var shape = document.getElementById('shape');
+
+function makeShapeReappear () {
+  shape.style.display = "block";
+  startTime = new Date().getTime();
+}
+
+setTimeout(makeShapeReappear (), 3000
+);
+
 
 
 function shapeDisplay () {
-  var shape = document.getElementById('shape');
+
 
   var endTime = new Date().getTime();
 
   var timeMeasure = document.getElementById('timeMeasure');
+
+
 
   if (shape.style.display === "none") {
     shape.style.display = "block";
   } else {
     shape.style.display = "none";
   }
+
+
 
   var timeTaken = (endTime - startTime) / 1000 + " seconds";
   console.log (timeTaken);
